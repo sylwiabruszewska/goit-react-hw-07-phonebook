7e0e17c4c5eee93e1900d6cb67201ea1e30111de
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-09-24 20:11:20 +0200

    remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// import { useEffect } from 'react';
-
 import {
   Container,
   Section,
@@ -9,27 +7,7 @@ import {
   InputField,
 } from './components/index';
 
-// const CONTACTS_LOCAL_STORAGE_KEY = 'contacts';
-
 export const App = () => {
-  // mounting phase - aktualizacja danych w state na podstawie danych w local storage
-  // useEffect(() => {
-  //   const storedContacts = JSON.parse(localStorage.getItem('contacts'));
-  //   if (storedContacts) {
-  //     setContacts(storedContacts);
-  //   }
-  // }, []);
-
-  // updating phase - aktualizacja danych w local storage przy update komponentu
-  // useEffect(() => {
-  //   const serializedContacts = JSON.stringify(contacts);
-  //   localStorage.setItem(CONTACTS_LOCAL_STORAGE_KEY, serializedContacts);
-
-  //   if (contacts.length === 0) {
-  //     localStorage.removeItem(CONTACTS_LOCAL_STORAGE_KEY);
-  //   }
-  // }, [contacts]);
-
   return (
     <Container>
       <h1>Phonebook</h1>
